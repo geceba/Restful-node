4a8be6d5c3f9eac2f8c807adeae25dfb89c6b741
Gerardo Cetzal <gerardoce23@gmail.com>
2019-06-24 23:40:00 -0500

    creando Schemas rutas

--- app.js
+++ app.js
@@ -5,19 +5,23 @@ var mongoose = require('mongoose');
 // Inicializar variables
 var app = express();
 
+// importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+
+
 // conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
     if(err) throw err;
 
     console.log('base de datos\x1b[32m%s\x1b[0m',' online')
 });
+
 // rutas
-app.get('/', ( req, res, next ) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: "Peticion realizada correctamente"
-    });
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/', appRoutes);
+
+
 // Escuchar peticiones
 app.listen(3000, () => {
     console.log('express server corriendo en el puerto 3000:\x1b[32m%s\x1b[0m',' online');
