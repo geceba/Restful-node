555a4a56f6e8ebad0b7cdfcc17d0ee9dce9164e5
Gerardo Cetzal <gerardoce23@gmail.com>
2019-06-25 15:31:56 -0500

    metodos crud

--- routes/usuario.js
+++ routes/usuario.js
@@ -52,4 +52,51 @@ app.post('/', (req, res) => {
 	});
 });
 
+// actualizar usuario
+
+app.put('/:id', (req, res) => {
+	var id = req.params.id;
+	var body = req.body;
+
+	console.log("body", body)
+	Usuario.findById(id, (err, usuario) => {
+		if (err) {
+			return res.status(500).json({
+				ok: false,
+				mensaje: 'Error: no se pudo buscar el usuario',
+				errros: err
+			});
+		}
+
+		if (!usuario) {
+			if (err) {
+				return res.status(400).json({
+					ok: false,
+					mensaje: 'Error: el usuario con el id: ' + id + ' no existe',
+					errros: { message: 'no existe usuario con ese id' }
+				});
+			}
+		}
+
+		usuario.nombre = body.nombre;
+		usuario.email = body.email;
+		usuario.role = body.role;
+
+		usuario.save((err, usuarioGuardado) => {
+			if (err) {
+				return res.status(400).json({
+					ok: false,
+					mensaje: 'Error: no se pudo actualizar el usuario',
+					errros: err
+				});
+			}
+
+			res.status(200).json({
+				ok: true,
+				usuario: usuarioGuardado
+			});
+		});
+	});
+});
+
 module.exports = app;
