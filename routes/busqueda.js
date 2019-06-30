ca765751bd320974e9eb48047f46853479d9a4aa
Gerardo Cetzal <gerardoce23@gmail.com>
2019-06-29 23:56:57 -0500

    uso del populate

--- routes/busqueda.js
+++ routes/busqueda.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var Hospital = require('../models/hospital');
 var Medico = require('../models/medico');
+var Usuario = require('../models/usuario');
 
 var app = express();
 
@@ -8,28 +9,70 @@ app.get('/todo/:busqueda', (req, res, next) => {
 	var busqueda = req.params.busqueda;
 
 	var regex  = new RegExp(busqueda, 'i');
+
+	Promise.all([ 
+		buscarHospitales(busqueda, regex), 
+		buscarMedicos(busqueda, regex),
+		buscarUsuario(busqueda, regex)])
+		.then(respuestas => {
+			res.status(200).json({
+				ok: true,
+				hospitales: respuestas[0],
+				medicos: respuestas[1],
+				usuarios: respuestas[2]
+			});	
+		})
 	
-	buscarHospitales( busqueda, regex).then( hospitales => {
-		res.status(200).json({
-			ok: true,
-			hospitales: hospitales
-		});
-	});
+	
 
 });
 
 function buscarHospitales( busqueda, regEx) {
 	
 	return new Promise( (resolve, reject) => {
-		Hospital.find({nombre: regEx}, (err, hospitales) => {
+		Hospital.find({nombre: regEx})
+		.populate('usuario', 'nombre email')
+		.exec( (err, hospitales) => {
+				if(err) {
+					reject('Error al cargar hospitales ', err);
+				} else {
+					resolve(hospitales);
+				}
+		});
+	});
+	
+}
+
+function buscarMedicos( busqueda, regEx) {
+	
+	return new Promise( (resolve, reject) => {
+		Medico.find({nombre: regEx})
+		.populate('usuario', 'nombre email')
+		.populate('hospital')
+		.exec((err, medicos) => {
 			if(err) {
-				reject('Error al cargar hospitales ', err);
+				reject('Error al cargar medicos ', err);
 			} else {
-				resolve(hospitales);
+				resolve(medicos);
 			}
 		});
 	});
 	
 }
 
+function buscarUsuario( busqueda, regex) {
+	
+	return new Promise( (resolve, reject) => {
+		Usuario.find({}, 'nombre email role')
+			.or([{'nombre': regex}, {'email': regex }])
+			.exec((err, usuarios) => {
+				if(err) {
+					reject('Error al enviar al cargar los usuarios');
+				} else {
+					resolve(usuarios)
+				}
+			})
+	});
+	
+}
 module.exports = app;
