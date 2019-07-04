1af1546134190388da195ed35dad11d9b85bd85f
Gerardo Cetzal <gerardoce23@gmail.com>
2019-07-04 13:46:35 -0500

    subida de imagenes y regresar imagen

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ var hospitalRoutes = require('./routes/hospital');
 var medicoRoutes = require('./routes/medico');
 var busquedaRoutes = require('./routes/busqueda');
 var uploadRoutes = require('./routes/upload');
+var imganesRoutes = require('./routes/imagenes');
 
 
 // conexion a la base de datos
@@ -37,6 +38,7 @@ app.use('/hospital', hospitalRoutes);
 app.use('/medico', medicoRoutes);
 app.use('/busqueda', busquedaRoutes);
 app.use('/upload', uploadRoutes);
+app.use('/img', imganesRoutes);
 app.use('/', appRoutes);
 
 
