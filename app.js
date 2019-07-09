9a80e9c0d44d6499822f371413425d087b18c046
Gerardo Cetzal <gerardoce23@gmail.com>
2019-07-09 13:23:55 -0500

    CORS

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ var bodyParser = require('body-parser');
 // Inicializar variables
 var app = express();
 
-// body parser 
+// CORS
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', "POST, GET, PUT, DELETE, OPTIONS");
+	next();
+});
 
+// body parser
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,12 +30,11 @@ var busquedaRoutes = require('./routes/busqueda');
 var uploadRoutes = require('./routes/upload');
 var imganesRoutes = require('./routes/imagenes');
 
-
 // conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
-    if(err) throw err;
+	if (err) throw err;
 
-    console.log('base de datos\x1b[32m%s\x1b[0m',' online')
+	console.log('base de datos\x1b[32m%s\x1b[0m', ' online');
 });
 
 // rutas
@@ -41,8 +47,7 @@ app.use('/upload', uploadRoutes);
 app.use('/img', imganesRoutes);
 app.use('/', appRoutes);
 
-
 // Escuchar peticiones
 app.listen(3000, () => {
-    console.log('express server corriendo en el puerto 3000:\x1b[32m%s\x1b[0m',' online');
+	console.log('express server corriendo en el puerto 3000:\x1b[32m%s\x1b[0m', ' online');
 });
